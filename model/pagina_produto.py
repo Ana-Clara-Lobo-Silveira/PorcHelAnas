7e85d089b94f9011af0a74cf96cc37bc2f717d0a
Ana Grifoni <ana.grifoni@aluno.senai.br>
2026-06-12 11:02:26 -0300

    terminando o jinja

--- model/pagina_produto.py
+++ model/pagina_produto.py
@@ -4,7 +4,7 @@ def pagina_produto():
     conexao,cursor = conectar()
     
     cursor.execute(""" SELECT nome_produto, descricao_produto, preco, imagem_produto, id_categoria
-                   FROM produtos WHERE = %s""")
+                   FROM produtos WHERE id_produto = 5;""")
 
     produto=cursor.fetchone()
 
