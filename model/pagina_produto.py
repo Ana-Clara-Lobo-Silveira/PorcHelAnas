d1cd6d23a0bf4e8b89f977b76a99e9d02fdb59c9
Ana Grifoni <ana.grifoni@aluno.senai.br>
2026-06-12 17:28:20 -0300

    resolvendo os comentarios

--- model/pagina_produto.py
+++ model/pagina_produto.py
@@ -3,7 +3,7 @@ def recupera_produto():
     conexao,cursor = Conexao.conexao()
     
     cursor.execute(""" SELECT nome_produto, descricao_produto, preco, imagem_produto, id_categoria
-                   FROM produtos WHERE id_produto = 4;""")
+                   FROM produtos WHERE id_produto = ;""")
 
     produto=cursor.fetchone()
 
