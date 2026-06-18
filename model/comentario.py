66e6f274e7e0d981114bfc6a4aea5d273c0bddc6
Ana Clara Lobo Silveira <ana.c.silveira15@aluno.senai.br>
2026-06-18 17:25:10 -0300

    ADD: Comentarios em andamento

--- model/comentario.py
+++ model/comentario.py
@@ -1,9 +1,9 @@
 from database.conexao import conectar
 
-def obter_comentario():
+def obter_comentario(id_produto):
     conexao, cursor = conectar()
     cursor.execute(
-            """ SELECT comentario, nome_completo FROM comentarios WHERE comentario =%s """
+            """ SELECT comentario, nome_completo FROM comentarios WHERE comentario id_produto=%s """, [id_produto]
     )
     comentarios = cursor.fetchall()
     cursor.close()
